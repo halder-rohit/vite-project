beeccb8cdbc992180a3791da297f2fba2d870ec9
halder-rohit <rohithalder324@gmail.com>
2025-04-06 18:10:36 +0530

    fevicon icon

--- react/vite-project/src/Components/Product.jsx
+++ react/vite-project/src/Components/Product.jsx
@@ -1,25 +1,10 @@
-import { Image, Image1 } from "./Image";
+import { Image1 } from "./Image";
 
 export function Product({productRef}) {
     return <div>
         <div className="space-y-24 pt-20" >
             <p className="flex justify-center text-4xl" ref={productRef}>Products</p>
              <div className="flex justify-around">
-                {/*<div className="flex-col justify-items-center">
-                    <img src='/Mushroom.jpg' className="h-28 w-30"></img>
-                    <p>Oyster Mushroom</p>
-                    <p>Available</p>
-                </div>
-                <div className="flex-col justify-items-center">
-                    <img src='/2.jpg' className="h-28 w-30"></img>
-                    <p>Paddy Straw Mushroom</p>
-                    <p>Available</p>
-                </div>
-                <div className="flex-col justify-items-center ">
-                    <img src='/3.jpeg' className="h-28 w-30"></img>
-                    <p>Button Mushroom</p>
-                    <p>Available</p>
-                </div> */}
                 <Image1 src='/Mushroom.jpg' name="Oyster Mushroom" description="Available"/>
                 <Image1 src='/2.jpg'  name="Paddy Straw Mushroom" description="Available"/>
                 <Image1 src='/3.jpeg'  name="Button Mushroom" description="Available"/>            
--- react/vite-project/src/Components/Team.jsx
+++ react/vite-project/src/Components/Team.jsx
@@ -2,8 +2,8 @@ import { Image } from "./Image";
 
 export function Team({teamRef}){
     return <div>
-        <div>
-                <p className="flex justify-around text" ref={teamRef}>Our Team</p>
+        <div className="space-y-24 pt-20" >
+                <p className="flex justify-around text text-4xl" ref={teamRef}>Our Team</p>
                 <div className="flex justify-around p-10">
                     <Image src="/pradhan.jpg" name="Abhijit Pradhan" link="https://www.linkedin.com/feed/"/>
                     <Image src="/partho.jpg" name="Debaprasad Dey" link="https://www.linkedin.com/feed/"/>
